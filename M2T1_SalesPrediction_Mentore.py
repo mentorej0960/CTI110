094829eb9a6737bc12a30f8dbe8fbdbfb7e24d3c
mentorej0960 <31388284+mentorej0960@users.noreply.github.com>
2017-08-27 11:42:07 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Mentore.py
@@ -0,0 +1,33 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore 2.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore 2.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore 2.py 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore 2.py 
+Enter the projected sales: 1000
+The profit is $ 230.0
+>>> 
+>>> 
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore 2.py 
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+ RESTART: C:/Users/Jermaine mentore/Documents/school/2017 Fall/CTI110/Class Submission/Module 2/M2T1_SalesPrediction_Mentore 2.py 
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
