69f578af4cb7801ddb80f9909676e7cc50a0792f
mentorej0960 <31388284+mentorej0960@users.noreply.github.com>
2017-09-16 06:40:10 -0400

    Add files via upload

---
+++ M3T1_AreasOfRectangles_JermineMentore.py
@@ -0,0 +1,23 @@
+# CTI-110
+# M3T1 - AreasOfRectangles
+# Jermine Mentore
+# September 16, 2017
+#.
+
+lengthR1= float( input("Please enter length of rectanagle 1: "))
+widthR1 = float( input("Please enter width of rectanagle 1: "))
+lengthR2= float( input("Please enter length of rectanagle 2: "))
+widthR2 = float( input("Please enter width of rectanagle 2: "))
+areaR1 = lengthR1 * widthR1
+areaR2 = lengthR2 * widthR2
+if areaR1 > areaR2:
+    print("Rectangle 1 is larger than Recatangle 2")
+else:
+     print("Rectange 2 is larger than rectangle 1")
+
+
+
+
+
+
+
