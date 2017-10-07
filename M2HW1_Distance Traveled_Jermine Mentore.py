9b381c3f2808705db8c364e77cc8ca43df879869
mentorej0960 <31388284+mentorej0960@users.noreply.github.com>
2017-10-07 13:02:25 -0400

    Delete M2HW1_Distance Traveled_Jermine Mentore.py

--- M2HW1_Distance Traveled_Jermine Mentore.py
+++
@@ -1,9 +0,0 @@
-# CTI-110
-#M2HW1- Distance Traveled
-#Jermine Mentore
-#August 27, 2017
-
-speed = 70
-print("In 6 hours, the car will travel" + str( ( speed * 6 ) ) + "miles" )
-print("In 10 hours, the car will travel" + str( ( speed * 10 ) ) + "miles" )
-print("In 15 hours, the car will travel" + str( ( speed * 15 ) ) + "miles" )
