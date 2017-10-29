3a1a8e9314aaf0e411b85a57907a26f0e768378f
mentorej0960 <31388284+mentorej0960@users.noreply.github.com>
2017-10-29 12:59:30 -0400

    Add files via upload

---
+++ M6T1_Kilometer converter_Mentore.py.py
@@ -0,0 +1,18 @@
+# CTI-110 
+# M6T1 - Kilometer Converter
+# Jermine Mentore
+# October 29, 2017
+#.
+
+
+kiloMeters = int( input( "\nPlease enter the number of kilometers: ")) 
+       
+conversion_factor = 0.621371
+
+miles = kiloMeters * conversion_factor
+print( 'Answer is equal to',miles,"miles") 
+
+
+
+
+
